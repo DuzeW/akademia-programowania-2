74805283a912dd849d77c6d3df32a3658f19708e
Wiktor <wiktor_laptop@example.com>
2024-03-27 12:49:52 +0100

    'zad1'

--- GoLang/zadania/academy/academy.go
+++ GoLang/zadania/academy/academy.go
@@ -1,5 +1,10 @@
 package academy
 
+import (
+	"math"
+	"sort"
+)
+
 type Student struct {
 	Name       string
 	Grades     []int
@@ -11,6 +16,14 @@ type Student struct {
 // slice containing all grades received during a
 // semester, rounded to the nearest integer.
 func AverageGrade(grades []int) int {
+	if len(grades) == 0 {
+		return 0
+	}
+	sum := 0
+	for i := 0; i < len(grades); i++ {
+		sum += grades[i]
+	}
+	return int(math.Round(float64(sum) / float64(len(grades))))
 	panic("not implemented")
 }
 
@@ -21,6 +34,16 @@ func AverageGrade(grades []int) int {
 // The percentage of attendance is represented as a
 // floating-point number ranging from 0 to 1.
 func AttendancePercentage(attendance []bool) float64 {
+	if len(attendance) == 0 {
+		return 0
+	}
+	sum := float64(0)
+	for i := 0; i < len(attendance); i++ {
+		if attendance[i] == true {
+			sum += 1
+		}
+	}
+	return sum / float64(len(attendance))
 	panic("not implemented")
 }
 
@@ -36,6 +59,15 @@ func AttendancePercentage(attendance []bool) float64 {
 // decreased by 1. If the student's attendance is below 60%, average
 // grade is 1 or project grade is 1, the final grade is 1.
 func FinalGrade(s Student) int {
+	sort.Ints(s.Grades)
+	if s.Project == 1 || s.Grades[0] == 1 || AttendancePercentage(s.Attendance) < 0.6 {
+		return 1
+	}
+	Final := int(math.Round((float64(s.Project) + float64(AverageGrade(s.Grades))) / 2))
+	if AttendancePercentage(s.Attendance) < 0.8 {
+		return Final - 1
+	}
+	return Final
 	panic("not implemented")
 }
 
@@ -43,5 +75,10 @@ func FinalGrade(s Student) int {
 // Student structs. The key is a student's name and the value is a
 // final grade.
 func GradeStudents(students []Student) map[string]uint8 {
+	m := map[string]uint8{}
+	for _, student := range students {
+		m[student.Name] = uint8(FinalGrade(student))
+	}
+	return m
 	panic("not implemented")
 }
